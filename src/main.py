4a2211c55b2babbda44174941a12b38a959315b1
Ana Gomez <anitaggrr@gmail.com>
2023-10-05 07:19:28 +0200

    feat:separation of X and y in the df

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
-from utils import load_data
+from utils import load_data, X_y_separation
 from dotenv import load_dotenv
 import os
 
 if __name__ == '__main__':
+    
+    #loading of .env variables
     load_dotenv()
     
     path_to_data = os.getenv('PATH_TO_DATA_CSV')
     
     df = load_data(path_to_data)
+
+    X, y = X_y_separation(df, 'stroke')
+
--- src/utils.py
+++ src/utils.py
@@ -19,3 +19,13 @@ def load_data(path_to_data):
     except Exception as e:
         print(f"Error al cargar el archivo CSV: {str(e)}")
         return None
+
+def X_y_separation(df, string_y):
+    '''
+    This function receives the dataframe and the name of the 
+    target/label column (as a string). It returns the X matrix 
+    and the y vector as two different variables.
+    '''
+    X = df.drop(string_y, axis = 1)
+    y = df[string_y].copy()
+    return X, y
