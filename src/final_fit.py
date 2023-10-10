d6daf4bcb50d840db1fea68bf903e660fda42a44
Ana Gomez <anitaggrr@gmail.com>
2023-10-10 15:30:36 +0200

    feat: documentation of metrics_final_training function

--- src/final_fit.py
+++ src/final_fit.py
@@ -4,8 +4,12 @@ import pickle
 from probability import false_positives_false_negatives
 from testing import test_evaluation
 
+
 def metrics_final_training(X, y):
     '''
+    This function takes de whole dataset separated as X and y.
+    It predicts y_predicted after passing X through the full_pipeline.
+    It also prints the metrics obtained from the call of test_evaluation.
     '''
     path_to_full_pipeline = os.getenv('PATH_TO_FULL_PIPELINE')
     with open (path_to_full_pipeline, 'rb') as file:
