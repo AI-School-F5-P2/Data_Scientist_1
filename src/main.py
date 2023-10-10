399e3c8a39730981b87829793a5346347c4a9c7b
Gala1812 <galynachupiryova@gmail.com>
2023-10-10 09:49:12 +0200

    correction of the last merge overwriting

--- src/main.py
+++ src/main.py
@@ -1,18 +1,16 @@
-import pandas as pd
 from utils import load_data, X_y_separation, split_stratify_y, create_test_set
-from preprocessing import create_preprocessor
+from preprocessing import create_fit_preprocessor
 from model import sampling_classifier_model, pickle_files
-from testing import transform_test_data, predict_test_data, test_evaluation
+from testing import transform_predict_test_data
 from dotenv import load_dotenv
 import os
+
 
 if __name__ == '__main__':
     
     #loading of .env variables
     load_dotenv()
-    
     path_to_data = os.getenv('PATH_TO_DATA_CSV')
-    
     df = load_data(path_to_data)
 
     X, y = X_y_separation(df, 'stroke')
@@ -21,16 +19,10 @@ if __name__ == '__main__':
 
     create_test_set(X_test, y_test)
 
-    preprocessor = create_preprocessor()
-
-    X_train_transformed = preprocessor.fit_transform(X_train)
+    preprocessor, X_train_transformed = create_fit_preprocessor(X_train)
 
     model = sampling_classifier_model(X_train_transformed, y_train)
 
     pickle_files(preprocessor, model)
-
-    X_test_transformed, y_test_true = transform_test_data()
-
-    y_test_predicted, y_probs = predict_test_data(X_test_transformed)
-
-    test_evaluation(y_test_true, y_test_predicted, y_probs)
+    
+    transform_predict_test_data()
