d18b2cd2af8eecf3526d5ec2f1c1fb295d1509c7
Ana Gomez <anitaggrr@gmail.com>
2023-10-10 07:20:25 +0200

    fix: transform_predict_test_data function instead of two functions separated

--- src/main.py
+++ src/main.py
@@ -2,17 +2,15 @@ import pandas as pd
 from utils import load_data, X_y_separation, split_stratify_y, create_test_set
 from preprocessing import create_preprocessor
 from model import sampling_classifier_model, pickle_files
-from testing import transform_test_data, predict_test_data, test_evaluation
+from testing import transform_test_data, transform_predict_test_data, predict_test_data, test_evaluation
 from dotenv import load_dotenv
 import os
 
 if __name__ == '__main__':
     
-    #loading of .env variables
+    #loading of .env variables and data
     load_dotenv()
-    
     path_to_data = os.getenv('PATH_TO_DATA_CSV')
-    
     df = load_data(path_to_data)
 
     X, y = X_y_separation(df, 'stroke')
@@ -29,8 +27,8 @@ if __name__ == '__main__':
 
     pickle_files(preprocessor, model)
 
-    X_test_transformed, y_test_true = transform_test_data()
+    # #X_test_transformed, y_test_true = transform_test_data()
 
-    y_test_predicted, y_probs = predict_test_data(X_test_transformed)
+    y_test_predicted, y_probs = transform_predict_test_data()
 
-    test_evaluation(y_test_true, y_test_predicted, y_probs)
+    # #test_evaluation(y_test_true, y_test_predicted, y_probs)
